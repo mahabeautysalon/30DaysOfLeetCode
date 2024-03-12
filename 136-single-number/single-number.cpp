81ef428313f140d136762611a5ff514742525021
Cyberspace <101250636+mahabeautysalon@users.noreply.github.com>
2024-03-12 09:09:45 +0500

    Time: 20 ms (27.80%) | Memory: 20.3 MB (28.23%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int count=1,unique=0;
+        sort(nums.begin(),nums.end());
+        if(nums.size()==1)
+        {
+            return nums[0];
+        }else if(nums[0]!=nums[1]){
+            return nums[0];
+        }else{
+            for(int i=0; i<nums.size(); i++)
+            {
+                if(nums[i]==nums[i+1])
+                {
+                    count++;
+                }else{
+                    if(count==1 && nums[i]!=nums[i+1])
+                    {
+                        unique=nums[i];
+                    }
+                    count=1;
+                }
+            }
+        }
+        return unique;
+    }
+};

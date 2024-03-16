93d8e7fd09ed96d6bf69d04a54179cf6490f6310
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-03-16 23:24:39 +0500

    Time: 17 ms (39.90%) | Memory: 20.3 MB (26.27%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -15,7 +15,7 @@ public:
                 {
                     count++;
                 }else{
-                    if(count==1 && nums[i]!=nums[i+1])
+                    if(count==1)
                     {
                         unique=nums[i];
                     }

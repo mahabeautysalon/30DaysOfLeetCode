55fed707f9cda30a2d86fd707e73218e79bfdc48
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-06-13 09:03:10 +0500

    Time: 11 ms (24.72%) | Memory: 10.9 MB (93.17%) - LeetSync

--- 1044-find-common-characters/find-common-characters.cpp
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -4,12 +4,12 @@ public:
         string temp="0";
         vector<string> res;
         bool check = true;
-        int pos=0;
-        for(int i=0; i<words[0].size(); i++)
+        int pos=0,n=words[0].size(),m=words.size();
+        for(int i=0; i<n; i++)
         {
             check = true;
             temp[0]=words[0][i];
-            for(int j=1; j<words.size(); j++)
+            for(int j=1; j<m; j++)
             {
                 pos = words[j].find(temp);
                 if(pos==string::npos)

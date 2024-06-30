9f373cc4c3188c90a159ad6fb4db37dcd7b1c177
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-06-30 13:07:38 +0500

    Time: 459 ms (15.60%) | Memory: 18.1 MB (97.53%) - LeetSync

--- 260-single-number-iii/single-number-iii.py
+++ 260-single-number-iii/single-number-iii.py
@@ -2,6 +2,9 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         res = []
         for val in nums:
-            if nums.count(val)==1:
+            if val not in res:
                 res.append(val)
+            else:
+                res.remove(val)
+            #print(res)
         return res

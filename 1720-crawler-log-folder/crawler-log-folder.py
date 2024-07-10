1a2eff6589921bb1c38b014ea4e8877d909dbc54
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-07-10 17:04:56 +0500

    Time: 83 ms (6.07%) | Memory: 17 MB (11.07%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.py
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -4,9 +4,8 @@ class Solution:
         for val in logs:
             if val[-2] == '.':
                 if len(val) > 2:
-                    if val[-3] == '.':
-                        if len(logsList) > 0:
-                            logsList.pop()
+                    if len(logsList) > 0:
+                        logsList.pop()
             else:
                 logsList.append(val)
             print(logsList)

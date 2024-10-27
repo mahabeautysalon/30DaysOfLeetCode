eec7088e42bfd09ab09bed6b0c0e96ba6607af59
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-10-27 10:44:26 +0500

    Time: 0 ms (100.00%) | Memory: 16.8 MB (73.03%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
+        int n =nums.size();
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] == target){
+            if(nums[i] == target || nums[n-i-1] == target){
                 return true;
             }
+            
         }
         return false;
     }

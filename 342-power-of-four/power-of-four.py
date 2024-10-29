d607fa831ad61f528ee5aaef3d1fa97d78ca9467
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-10-29 23:40:23 +0500

    Time: 0 ms (100.00%) | Memory: 16.7 MB (13.47%) - LeetSync

---
+++ 342-power-of-four/power-of-four.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        elif n == 1:
+            return True
+        def power(n,exp):
+            print(f" n : {n}")
+            print(f"exp * 4 : {4*exp}")
+            if n < 4*exp:
+                return False
+            elif n == 4*exp:
+                return True
+            else:
+                exp = exp*4
+                return power(n,exp)
+        return power(n,1)

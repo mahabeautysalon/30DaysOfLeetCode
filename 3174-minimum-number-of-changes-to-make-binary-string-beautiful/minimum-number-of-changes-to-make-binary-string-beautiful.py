b8e1273b086180cf0b485b416ebe861b3c529148
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-11-05 19:13:21 +0500

    Time: 63 ms (5.40%) | Memory: 20.5 MB (6.10%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -9,8 +9,5 @@ class Solution:
             return 0
         else:
             result = 0
-            substrings = re.findall('.{1,2}', s)
-            for val in substrings:
-                if(len(set(val)) == 2):
-                    result += 1
-        return result
+            substrings = [s[i:i+2] for i in range(0, len(s), 2) if(len(set(s[i:i+2])) == 2)]
+        return len(substrings)

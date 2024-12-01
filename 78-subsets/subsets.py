6038ee06609cffe1fc572a1e968a96c1ded5aac9
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-12-01 21:53:06 +0500

    Time: 99 ms (5.08%) | Memory: 17.5 MB (5.24%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -3,15 +3,16 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         self.ans = []
         def sets(nums,arr):
-            print(arr)
             if(not nums):
                 if arr not in self.ans:
                     self.ans.append(copy.deepcopy(arr))
                 return
             for i in range(len(nums)):
-                sets(nums[i+1:],copy.deepcopy(arr))
+                sets(nums[i+1:],arr)
                 arr.append(nums[i])
-                sets(nums[i+1:],copy.deepcopy(arr))
-        
+                sets(nums[i+1:],arr)
+                #print(f"before pop : {arr}")
+                arr.pop()
+                #print(f"afer pop : {arr}")
         sets(nums,[])
         return self.ans

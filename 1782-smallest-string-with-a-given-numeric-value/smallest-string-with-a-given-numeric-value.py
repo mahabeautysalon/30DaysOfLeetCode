b0889cc85d54335eee6e602f69638b2f8d39d616
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-12-01 16:52:49 +0500

    Time: 3 ms (89.89%) | Memory: 18.3 MB (51.39%) - LeetSync

---
+++ 1782-smallest-string-with-a-given-numeric-value/smallest-string-with-a-given-numeric-value.py
@@ -0,0 +1,33 @@
+from bisect import bisect_left
+class Solution:
+    def getSmallestString(self, n: int, k: int) -> str:
+        alpha = [
+                    "a","b","c","d","e","f","g",
+                    "h","i","j","k","l","m","n","o",
+                    "p","q","r","s","t","u","v","w",
+                    "x","y","z"
+                ]
+        temp = str()
+        val, quotient, module = 26, 1, 1
+        while(n>0):
+            if(k-(n-1)<= 26):
+                return alpha[0]*(n-1) + alpha[k-n] + temp
+            else:
+                quotient = k // val
+                module = k % val
+                n -= quotient
+                if(module < n):
+                    while(n>module):
+                        n += 1
+                        quotient -= 1
+                        module += 26
+                temp = alpha[val-1]*quotient + temp
+                k = module
+                
+            """print(f"n : {n}")
+            print(f"k : {k}")
+            print(f"val : {val}")
+            print(f"quotient : {quotient}")
+            print(f"module : {module}")
+            print(f"temp  : {temp}")"""
+        return temp

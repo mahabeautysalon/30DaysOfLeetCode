bfc12b73f2e2627593c775abd23db412d04378c4
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-12-26 22:48:01 +0500

    Time: 13 ms (13.89%) | Memory: 17.8 MB (5.57%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,15 +1,11 @@
 from collections import deque
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        q = deque(tickets)
         n = len(tickets)
-        ans = 0
-        ind = k
-        while(q[k] > 0 and len(q) > 0):
-            val = q.popleft()
-            if(val > 0):
-                val -= 1
+        ans, i = 0, 0
+        while(tickets[k] > 0):
+            if tickets[i] > 0:
+                tickets[i] -= 1
                 ans += 1
-            q.append(val)
-            k = (k-1)%n
+            i = (i+1) % n
         return ans

ea77ad01e61271aa03ab67417cb4734569f5bfa9
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-12-30 23:20:10 +0500

    Time: 39 ms (18.24%) | Memory: 26.1 MB (21.19%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,14 +3,11 @@ class Solution:
         n = len(nums)
         prefix, sufix = [1]*n, [1]*n
         ans = [0]*n
+        right = n-2
         for i in range(1, n):
             prefix[i] = prefix[i-1] * nums[i-1]
-        for i in range(n-2, -1, -1):
-            sufix[i] = sufix[i+1]*nums[i+1]
-        #print(prefix)
-        #print(sufix)
-        # [1, 1, 2, 6]
-        # [24, 12, 4, 1]
+            sufix[right] = sufix[right+1]*nums[right+1]
+            right -= 1
         for i in range(n):
             ans[i] = prefix[i]*sufix[i]
         return ans
